abf91b7aaa5f8e07f1fd1a22a80faba405fbd36e
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-04-12 21:18:54 +0700

    Update set omitempty to password field

--- models/user.go
+++ models/user.go
@@ -7,7 +7,7 @@ type User struct {
 	FirstName string    `json:"firstName"`
 	LastName  string    `json:"lastName"`
 	Email     string    `json:"email"`
-	Password  string    `json:"password"`
+	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
