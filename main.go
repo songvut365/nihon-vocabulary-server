e4a30aa97aa892a214149de3d4246963a5cc441d
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-04-14 21:29:20 +0700

    Setup swagger

--- main.go
+++ main.go
@@ -4,10 +4,25 @@ import (
 	"nihon-vocabulary/configs"
 	"nihon-vocabulary/routes"
 
+	_ "nihon-vocabulary/docs"
+
+	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+// @title Nihon Vocabulary
+// @version 1.0
+// @description This is an API for Nihon Vocabulary Application
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name ApiKeyAuth
+
+// @contact.name Songvut Nakrong
+// @contact.email songvut.nakrong@gmail.com
+
+// @BasePath /api/v1/
 func main() {
 	app := fiber.New()
 	app.Use(logger.New())
@@ -18,9 +33,11 @@ func main() {
 	//connect database
 	configs.ConnectDB()
 
-	//setup routes
+	//setup routesapp
 	api := app.Group("/api")
 	v1 := api.Group("/v1")
+
+	v1.Get("/docs/*", swagger.HandlerDefault) //swagger PATH api/v1/docs/
 
 	routes.AuthenticationRoutes(v1) // PATH api/v1/auth
 	routes.UserRoutes(v1)           // PATH api/v1/user
